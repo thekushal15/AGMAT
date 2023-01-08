c5ef0069b47d8d76df4a2c8d6ff9fb7e1684301e
thekushal15 <thekushal15@gmail.com>
2023-01-08 11:27:34 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { NavBar } from "./components/NavBar";
+import { Projects } from "./components/Projects";
+import { Footer } from "./components/Footer";
+import Intro from "./components/Intro";
+import { Projects2 } from "./components/Projects2";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Intro />
+      <Projects />
+      <Projects2 />
+      <Footer />
     </div>
   );
 }
